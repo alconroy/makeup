e0ea8dac9b3bac32d16285750c31472c30c0f376
Shane Kelly <shanekweb@icloud.com>
2016-05-31 14:35:19 +0100

    init

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,18 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',                        # required
+    :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],     # required
+    :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],    # required
+    :region                 => 'Ireland',                  # optional, defaults to 'us-east-1'
+    :host                   => 's3.example.com',             # optional, defaults to nil
+    :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+  }
+  config.fog_directory  = 'recharge-cartridges'                          # required
+  config.fog_public     = false                                        # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>"max-age=#{365.day.to_i}"} # optional, defaults to {}
+end
+
+class AvatarUploader < CarrierWave::Uploader::Base
+  include CarrierWaveDirect::Uploader
+end
+
